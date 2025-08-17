d4940c4403500e582d24eb8d4dad0af8a7336f5d
Jupiter Bania <jupiterbania472@gmail.com>
2025-08-17 09:04:52 +0000

    url pe jo logo dikhta hai wo v save websit ka name ke logo ka jsa bna do

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="https://iili.io/Fijeoxf.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
