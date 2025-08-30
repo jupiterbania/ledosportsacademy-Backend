96b9ec57618106ea068f7bb591d70ed36a7e3fe9
Jupiter Bania <jupiterbania472@gmail.com>
2025-08-30 04:52:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Toaster } from '@/components/ui/toaster';
 import { AuthProvider } from '@/components/auth/auth-provider';
 import { Header } from '@/components/layout/header';
 import { ThemeProvider } from "@/components/theme-provider"
-import { SpaceGrotesk } from 'next/font/google'
+import { Space_Grotesk as SpaceGrotesk } from 'next/font/google'
 import { Background } from '@/components/background';
 
 const spaceGrotesk = SpaceGrotesk({
